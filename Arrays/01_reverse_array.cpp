2ab5018e86662f0f961707167b0a1f698e124e4f
Deesha  Dhabale <deeshadhabale@gmail.com>
2026-03-22 14:45:52 +0530

    Create 01_reverse_array.cpp

---
+++ Arrays/01_reverse_array.cpp
@@ -0,0 +1,22 @@
+// Q1 - Reverse an Array
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+    
+    // Reverse
+    int start = 0, end = n - 1;
+    while(start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    
+    // Print
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    
+    return 0;
+}
